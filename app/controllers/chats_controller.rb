b74efe67abae06365892a8ef56629907d0843280
kei-sasaki-rgb-cell <11eftx2tree@gmail.com>
2021-09-15 21:15:48 +0900

    chatapp準備

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,30 +3,9 @@ class ChatsController < ApplicationController
     @chats = Chat.all.order(id: "DESC")
   end
 
-  def new
-    @chat = Chat.new
-  end
-
   def create
-    chat = Chat.create(content: params[:content], checked: false)
-    render json:{ chat: chat}
-  end
-
-  def checked
-    chat = Chat.find(params[:id])
-    if chat.checked
-      chat.update(checked: false)
-    else
-      chat.update(checked: true)
-    end
-
-    item = Chat.find(params[:id])
-    render json:{ chat: item }
-  end
-
-  private
-  def chat_params
-    params.require(:chat).permit(:name, :text)
+    Chat.create(content: params[:content])
+    redirect_to action: :index
   end
 
 end
